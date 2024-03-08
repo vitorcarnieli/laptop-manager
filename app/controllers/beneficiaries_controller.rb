c8246c55c8966db60230637be4fdff12ab6f7450
vitorcarnieli <vitorcarnieli135@gmail.com>
2024-03-07 22:30:40 -0300

    new beneficiary

--- app/controllers/beneficiaries_controller.rb
+++ app/controllers/beneficiaries_controller.rb
@@ -6,4 +6,24 @@ class BeneficiariesController < ApplicationController
   def show
     @beneficiary = Beneficiary.find(params[:id])
   end
+
+  def new
+    @beneficiary = Beneficiary.new
+  end
+
+  def create
+    @beneficiary = Beneficiary.new(beneficiary_params)
+
+    if @beneficiary.save
+      redirect_to @beneficiary
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+    def beneficiary_params
+      params.require(:beneficiary).permit(:name, :identification_document, :contact_number, :contract_type)
+    end
 end
--- app/models/beneficiary.rb
+++ app/models/beneficiary.rb
@@ -1,2 +1,7 @@
 class Beneficiary < ApplicationRecord
+  validates :name, presence: true
+  validates :identification_document, presence: true, length: { minimum: 9, maximum: 11 }
+  validates :contact_number, presence: true
+  validates :contract_type, presence: true, inclusion: { in: %w(contratado efetivo) }
+  
 end
